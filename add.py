c7b196fc6b984595dd35a1a1448edaa3a64521a5
Пётр Сивунов <70653232+Petrino411@users.noreply.github.com>
2023-10-02 22:43:40 +0300

    Initial commit

---
+++ add.py
@@ -0,0 +1,119 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtWidgets import QWidget
+
+
+class Add(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setObjectName("Form")
+        self.resize(351, 300)
+        self.setFixedSize(self.size())
+        font = QtGui.QFont()
+        font.setFamily("Calibri Light")
+        font.setPointSize(9)
+        self.setFont(font)
+        self.setStyleSheet("background-color: rgb(23, 33, 43);")
+        self.layoutWidget = QtWidgets.QWidget(parent=self)
+        self.layoutWidget.setGeometry(QtCore.QRect(30, 10, 294, 275))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.layoutWidget.setFont(font)
+        self.layoutWidget.setObjectName("layoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.layoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.frame = QtWidgets.QFrame(parent=self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.frame.setFont(font)
+        self.frame.setObjectName("frame")
+        self.formLayout = QtWidgets.QFormLayout(self.frame)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(parent=self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label)
+        self.label_2 = QtWidgets.QLabel(parent=self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label_2)
+        self.lineEdit = QtWidgets.QLineEdit(parent=self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                    " border: 1px solid rgb(35, 46, 60);\n"
+                                    "     border-radius:7px;")
+        self.lineEdit.setObjectName("lineEdit")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.ItemRole.FieldRole, self.lineEdit)
+        self.label_3 = QtWidgets.QLabel(parent=self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_3.setObjectName("label_3")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.ItemRole.LabelRole, self.label_3)
+        self.textEdit = QtWidgets.QTextEdit(parent=self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.textEdit.setFont(font)
+        self.textEdit.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                    " border: 1px solid rgb(35, 46, 60);\n"
+                                    "     border-radius:7px;")
+        self.textEdit.setObjectName("textEdit")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.ItemRole.FieldRole, self.textEdit)
+        self.dateEdit = QtWidgets.QDateEdit(parent=self.frame)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.dateEdit.setFont(font)
+        self.dateEdit.setStyleSheet("color: rgb(255, 255, 255);\n"
+                                    " border: 1px solid rgb(35, 46, 60);\n"
+                                    "     border-radius:7px;")
+        self.dateEdit.setObjectName("dateEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.ItemRole.FieldRole, self.dateEdit)
+        self.verticalLayout.addWidget(self.frame)
+        self.pushButton = QtWidgets.QPushButton(parent=self.layoutWidget)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("QPushButton{\n"
+                                        "    color: rgb(255, 255, 255);\n"
+                                        "     background-color:rgb(24, 37, 51);\n"
+                                        "     border: 1px solid rgb(35, 46, 60);\n"
+                                        "     border-radius:7px;\n"
+                                        "width: 230;\n"
+                                        "height: 50;\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "}\n"
+                                        "QPushButton:pressed{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "};")
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("Form", "Add"))
+        self.label.setText(_translate("Form", "Date:"))
+        self.label_2.setText(_translate("Form", "Title:"))
+        self.label_3.setText(_translate("Form", "Notes:"))
+        self.pushButton.setText(_translate("Form", "Add"))
---
+++ todo.py
@@ -0,0 +1,214 @@
+from PyQt6.QtWidgets import QMainWindow
+from PyQt6 import QtCore, QtGui, QtWidgets
+from add import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setObjectName("MainWindow")
+        self.resize(631, 610)
+        self.setMouseTracking(True)
+        self.setFixedSize(self.size())
+        self.setStyleSheet("background-color: rgb(23, 33, 43);\n"
+                                "color: rgb(226, 226, 226);")
+        self.centralwidget = QtWidgets.QWidget(parent=self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.widget = QtWidgets.QWidget(parent=self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(30, 10, 587, 248))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.calendarWidget = QtWidgets.QCalendarWidget(parent=self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.calendarWidget.setFont(font)
+        self.calendarWidget.setStyleSheet("background-color: rgb(23, 33, 43);\n"
+                                        "alternate-background-color: rgb(35, 46, 60);\n"
+                                        "")
+        self.calendarWidget.setLocale(QtCore.QLocale(QtCore.QLocale.Language.English, QtCore.QLocale.Country.UnitedKingdom))
+        self.calendarWidget.setObjectName("calendarWidget")
+        self.horizontalLayout_3.addWidget(self.calendarWidget)
+        self.line = QtWidgets.QFrame(parent=self.widget)
+        self.line.setFrameShape(QtWidgets.QFrame.Shape.VLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.line.setObjectName("line")
+        self.horizontalLayout_3.addWidget(self.line)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(parent=self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.label_3 = QtWidgets.QLabel(parent=self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_3.setFont(font)
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout.addWidget(self.label_3)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.label_6 = QtWidgets.QLabel(parent=self.widget)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.verticalLayout.addWidget(self.label_6)
+        self.listWidget = QtWidgets.QListWidget(parent=self.widget)
+        self.listWidget.setStyleSheet(" border: 1px solid rgb(35, 46, 60);\n"
+                                        "     border-radius:7px;")
+        self.listWidget.setObjectName("listWidget")
+        self.verticalLayout.addWidget(self.listWidget)
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+        self.widget1 = QtWidgets.QWidget(parent=self.centralwidget)
+        self.widget1.setGeometry(QtCore.QRect(20, 280, 591, 317))
+        self.widget1.setObjectName("widget1")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.widget1)
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.line_2 = QtWidgets.QFrame(parent=self.widget1)
+        self.line_2.setFrameShape(QtWidgets.QFrame.Shape.HLine)
+        self.line_2.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        self.line_2.setObjectName("line_2")
+        self.verticalLayout_2.addWidget(self.line_2)
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label_2 = QtWidgets.QLabel(parent=self.widget1)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_2.addWidget(self.label_2)
+        self.label_4 = QtWidgets.QLabel(parent=self.widget1)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_4.setFont(font)
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        self.horizontalLayout_2.addWidget(self.label_4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.label_5 = QtWidgets.QLabel(parent=self.widget1)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_2.addWidget(self.label_5)
+        self.textBrowser = QtWidgets.QTextBrowser(parent=self.widget1)
+        self.textBrowser.setStyleSheet(" border: 1px solid rgb(35, 46, 60);\n"
+                                        "     border-radius:7px;")
+        self.textBrowser.setObjectName("textBrowser")
+        self.verticalLayout_2.addWidget(self.textBrowser)
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.pushButton_3 = QtWidgets.QPushButton(parent=self.widget1)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.pushButton_3.setFont(font)
+        self.pushButton_3.setStyleSheet("QPushButton{\n"
+                                        "    color: rgb(255, 255, 255);\n"
+                                        "     background-color:rgb(24, 37, 51);\n"
+                                        "     border: 1px solid rgb(35, 46, 60);\n"
+                                        "     border-radius:7px;\n"
+                                        "width: 230;\n"
+                                        "height: 50;\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "}\n"
+                                        "QPushButton:pressed{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "};")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout_4.addWidget(self.pushButton_3)
+        self.pushButton_2 = QtWidgets.QPushButton(parent=self.widget1)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("QPushButton{\n"
+                                        "    color: rgb(255, 255, 255);\n"
+                                        "     background-color:rgb(24, 37, 51);\n"
+                                        "     border: 1px solid rgb(35, 46, 60);\n"
+                                        "     border-radius:7px;\n"
+                                        "width: 230;\n"
+                                        "height: 50;\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "}\n"
+                                        "QPushButton:pressed{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "};")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_4.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(parent=self.widget1)
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("QPushButton{\n"
+                                        "    color: rgb(255, 255, 255);\n"
+                                        "     background-color:rgb(24, 37, 51);\n"
+                                        "     border: 1px solid rgb(35, 46, 60);\n"
+                                        "     border-radius:7px;\n"
+                                        "width: 230;\n"
+                                        "height: 50;\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "}\n"
+                                        "QPushButton:pressed{\n"
+                                        "background-color:rgb(35, 46, 60);\n"
+                                        "};")
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout_4.addWidget(self.pushButton)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_4)
+        self.setCentralWidget(self.centralwidget)
+
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("MainWindow", "ToDoList"))
+        self.label.setText(_translate("MainWindow", "Current date:"))
+        self.label_6.setText(_translate("MainWindow", "Plans:"))
+        self.label_2.setText(_translate("MainWindow", "Current matter:"))
+        self.label_5.setText(_translate("MainWindow", "Description:"))
+        self.pushButton_3.setText(_translate("MainWindow", "Remove"))
+        self.pushButton_2.setText(_translate("MainWindow", "Edit"))
+        self.pushButton.setText(_translate("MainWindow", "Add"))
+
+        self.calendarWidget.selectionChanged.connect(self.dateview)
+        self.label.setText('Current date: ' + str(self.calendarWidget.selectedDate().toPyDate()))
+        self.pushButton.clicked.connect(self.add)
+
+        self.msg = Add()
+
+
+
+
+    def dateview(self):
+        self.label.setText('Current date: ' + str(self.calendarWidget.selectedDate().toPyDate()))
+
+    def add(self):
+        self.msg.show()
+        self.blockSignals(True)
+
+
